1ca14ff804d47d78fbc0767065212d394cb9dfa4
yoo9519 <yoo9519@gmail.com>
2021-09-18 02:04:45 +0900

    modified pyspark example script

--- Spark/example/intro.py
+++ Spark/example/intro.py
@@ -20,14 +20,3 @@ peopleDF.createOrReplaceTempView("people")
 
 teenagerNamesDF = spark.sql("SELECT name FROM people WHERE age BETWEEN 13 AND 19")
 teenagerNamesDF.show()
-
-# json 파일 읽어들이기
-path = '/Users/younghun/Desktop/gitrepo/TIL/pyspark/people.json'
-df = spark.read.json(path)
-
-# Global Temporary View 생성
-df.createOrReplaceGlobalTempView('people')
-
-# 'global_temp' 라는 키워드 꼭 붙여주자!
-sqlDF = spark.sql('SELECT * FROM global_temp.people')
-sqlDF.show()

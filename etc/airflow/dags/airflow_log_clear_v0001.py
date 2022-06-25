3ef0aa071efc834fe51455dd694af02a7b050379
clyne <yoo9519@gmail.com>
2022-06-25 17:53:25 +0900

    airflow_init setting

---
+++ etc/airflow/dags/airflow_log_clear_v0001.py
@@ -0,0 +1,47 @@
+import pendulum
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator  # Import BashOperator
+
+#######################################
+############### Set ENV ###############
+#######################################
+local_tz = pendulum.timezone("Asia/Seoul")
+
+default_args = {
+    'owner': 'Clyne',
+    'email': 'yoo9519@gmail.com',
+    'retries': 3,
+    'retry_delay': timedelta(minutes=10)
+}
+
+dag = DAG(
+    dag_id='airflow_log_clear_v0001',
+    default_args=default_args,
+    start_date=datetime(2022, 6, 23, tzinfo=KST_TZ),
+    catchup=False,
+    schedule_interval='@daily',  # Crontime : min hour day month week / 매일 02시에 삭제
+    max_active_runs=3,
+    tags=['operation']
+)
+
+########################################
+############### DAG Task ###############
+########################################
+start = DummyOperator(
+    dag=dag,
+    task_id='Start'
+)
+
+del_log = BashOperator(
+    task_id='clear_log',
+    bash_command='find /Users/yoo/Data-dev/etc/airflow/logs -type f -mtime +2 -delete',
+    dag=dag
+)
+
+
+##############################################
+############### Task operation ###############
+##############################################
+start >> del_log

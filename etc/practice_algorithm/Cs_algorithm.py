7ec9f01376721a7098768cbc8df8e89410871e67
clyne <yoo9519@gmail.com>
2022-06-25 17:54:00 +0900

    cs_algorithm main

---
+++ etc/practice_algorithm/Cs_algorithm.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+print(f"PATH: {sys.path}", f"\npython: {sys.executable}")
+
+    
+
+def main():
+    pass
+
+    
+
+if __name__ == "__main__":
+    main()
